6c190ffad523c88ad3ae8ca4f7781c084ece7164
GrigLog <griglogvin@gmail.com>
2022-07-10 21:30:47 +0300

    Start

---
+++ src/main/java/arcana/Arcana.java
@@ -0,0 +1,11 @@
+package arcana;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Arcana.id)
+public class Arcana {
+    public static final String id = "arcana";
+    public static final Logger logger = LogManager.getLogger();
+}
